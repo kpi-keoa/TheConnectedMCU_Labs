dbee1cdb5a351e4c756926d636010f288197d3c2
Alexandr Skopets <skopets.sasha@gmail.com>
2017-10-04 05:09:19 +0300

    Initial implementation of the first laboratory project. README.md and .gitignore updated

---
+++ alexskp/lab1-BasicIO/main.c
@@ -0,0 +1,8 @@
+
+#include "lab1_basicIO.h"           /* User funct/params, such as InitApp             */
+
+int32_t main(void)
+{
+    init();     /* initializing GPIOs */  
+    blinker();  /* blinker application */
+}
