93584a769ab381a69b2ca3345ca4d5b0a2fa83a1
Maxim2222 <maxim23.95@gmail.com>
2017-10-17 19:47:16 +0300

    Add files via upload

---
+++ maxim2222/lab2/user.h
@@ -0,0 +1,24 @@
+/******************************************************************************/
+/* User Level #define Macros                                                  */
+/******************************************************************************/
+
+// Add I/O name macros here
+#define LD1_PORT_BIT        LATGbits.LATG6
+#define LD2_PORT_BIT        LATDbits.LATD4
+#define LD3_PORT_BIT        LATBbits.LATB11
+#define LD4_PORT_BIT        LATGbits.LATG15
+
+#define BTN1_PORT_BIT       PORTAbits.RA5
+#define BTN2_PORT_BIT       PORTAbits.RA4
+
+/* TODO Application specific user parameters used in user.c may go here */
+#define FAST_DELAY          1000000
+
+/******************************************************************************/
+/* User Function Prototypes                                                    /
+/******************************************************************************/
+
+/* TODO User level functions prototypes (i.e. InitApp) go here */
+
+void InitApp(void);         /* I/O and Peripheral Initialization */
+void Run_Game_Tasks(void);
