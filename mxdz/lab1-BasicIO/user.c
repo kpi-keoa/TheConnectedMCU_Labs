ab483de28fde6efff9f8c246dca8c5e078c72161
mxdz <maks@airmail.cc>
2017-10-26 23:57:01 +0300

    requested changes and clean up

--- mxdz/lab1-BasicIO/user.c
+++ mxdz/lab1-BasicIO/user.c
@@ -1,11 +1,14 @@
 #include "user.h"      
 
-void morse_code_encoding(char msg[], char *res) {
-    char *morse_table[] = {
-        "13","3111","3131","311","1","1131","331","1111","11","1333","313","1311","33","31","333","1331","3313","131","111","3","113","1113","133","3113","3133","3311"
+void morse_code_encoding(uint8_t msg[], uint8_t *res) 
+{
+    uint8_t *morse_table[] = {
+        "13","3111","3131","311","1","1131","331","1111","11","1333",
+        "313","1311","33","31","333","1331","3313","131","111",
+        "3","113","1113","133","3113","3133","3311"
     };
     for (uint32_t i = 0; msg[i]!='\0'; i++) {
-        if (msg[i] == ' ') {
+        if (' ' == msg[i]) {
             strcat(res, " ");
         } else {
             int pos = msg[i] - 'a';
@@ -18,24 +21,19 @@ void init_app(void) {
     /* Setup analog functionality and port direction */
 
     // LED output
-    // Disable analog mode for G6
-    ANSELGbits.ANSG6 = 0;
-    // Set direction to output for G6
-    TRISGbits.TRISG6 = 0;
+    // Disable analog mode for G6 and G15
+    ANSELG &= ~((1 << 6) | (1 << 15));
+
+    // Set direction to output for G6 and G15
+    TRISG &= ~((1 << 6) | (1 << 15));
 
     // Initialize outputs for other LEDs
     ANSELBbits.ANSB11 = 0;
-    ANSELGbits.ANSG15 = 0;
-
     TRISBbits.TRISB11 = 0;
-    TRISGbits.TRISG15 = 0;
     TRISDbits.TRISD4 = 0;
 
     // Turn off LEDs for initialization
-    LED_1 = 0;
-    LED_2 = 0;
-    LED_3 = 0;
-    LED_4 = 0;
+    LED_1 = LED_2 = LED_3 = LED_4 = 0;
 
     // BTN1 input
     // Disable analog mode
@@ -54,12 +52,12 @@ void delay(volatile uint32_t val) {
 }
 
 void blink_leds(uint32_t mode) {
-    if (mode == 1) {
+    if (1 == mode) {
         LED_1 = 1;
         delay(BLINK_DELAY);
         LED_1 = 0;
         delay(BLINK_DELAY);
-    } else if(mode == 3) {
+    } else if(3 == mode) {
         LED_2 = LED_3 = LED_4 = 1;
         delay(BLINK_DELAY);
         LED_2 = LED_3 = LED_4 = 0;
@@ -68,12 +66,12 @@ void blink_leds(uint32_t mode) {
 }
 
 void display_msg(void) {
-    if (BTN_1 == 1) { 
+    if (BTN_1) { 
         delay(DEBOUNCE_DELAY);
         for(uint32_t i = 0; encoded_msg[i]!='\0'; i++) {
-            if(encoded_msg[i] == '1') {
+            if('1' == encoded_msg[i]) {
                 blink_leds(1);
-            } else if(encoded_msg[i] == '3') {
+            } else if('3' == encoded_msg[i]) {
                 blink_leds(3);
             } else {
                 delay(BLINK_DELAY);
--- mxdz/lab1-BasicIO/user.h
+++ mxdz/lab1-BasicIO/user.h
@@ -6,22 +6,23 @@
 
 #include <stdint.h>       
 #include <string.h>
-#define LED_1        LATGbits.LATG6
-#define LED_2       LATDbits.LATD4
-#define LED_3        LATBbits.LATB11
-#define LED_4      LATGbits.LATG15
 
-#define BTN_1       PORTAbits.RA5
-#define BTN_2       PORTAbits.RA4
+#define LED_1 LATGbits.LATG6
+#define LED_2 LATDbits.LATD4
+#define LED_3 LATBbits.LATB11
+#define LED_4 LATGbits.LATG15
+
+#define BTN_1 PORTAbits.RA5
+#define BTN_2 PORTAbits.RA4
 
 #define BLINK_DELAY 10000000
 #define DEBOUNCE_DELAY 2000000
-#define MSG "test msg"
 
-char encoded_msg[100];
+const uint8_t MSG[] = "test msg";
+uint8_t encoded_msg[100];
 
 void delay(volatile uint32_t val);
 void init_app(void);         // I/O and Peripheral Initialization 
 void blink_leds(uint32_t mode);
 void display_msg(void);
-void morse_code_ecoding(char msg[], char *res);
+void morse_code_ecoding(uint8_t msg[], uint8_t *res);
